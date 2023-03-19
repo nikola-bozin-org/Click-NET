e097a24380012224140882b0482f55d02a07df20
nikola.bozin.org@outlook.com <nikola.bozin.org@outlook.com>
2023-03-19 07:21:03 +0100

    removed cors

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const server = express();
 const port = 9876;
 
 server.use(cors({
-  origin: 'http://localhost:3000',
+  // origin: 'http://localhost:3000',
 }));
 server.use(express.json()); 
 server.use(express.urlencoded({ extended: true })); 
