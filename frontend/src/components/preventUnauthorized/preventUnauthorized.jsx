25ec446249dd8cd496f3e5ac4c34a7636af58d36
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-03-24 16:04:47 +0100

    change

--- frontend/src/components/preventUnauthorized/preventUnauthorized.jsx
+++ frontend/src/components/preventUnauthorized/preventUnauthorized.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Unauthorized from '../unauthorized/Unauthorized'
 import unauthorized from '../../images/unauthorized.png'
+import { useEffect } from 'react';
 
 
 const preventUnauthorized = (WrappedComponent) => {
@@ -8,12 +9,68 @@ const preventUnauthorized = (WrappedComponent) => {
   return (props) => {
     const accessToken = localStorage.getItem('accessToken');
     
-    if (!accessToken) {
-      return <Unauthorized imageUrl={unauthorized} />;
-    }
+    useEffect(()=>{
+      if (!accessToken) {
+        return <Unauthorized imageUrl={unauthorized} />;
+      }
+      console.info("calledOnce")
+    },[])
+
+
 
     return <WrappedComponent {...props} />;
   };
 };
 
 export default preventUnauthorized;
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import Unauthorized from './Unauthorized';
+
+// const preventUnauthorized = (WrappedComponent) => {
+//   const imageUrl = "https://your-image-url-here.com/image.jpg";
+
+//   return (props) => {
+//     const [status, setStatus] = useState('loading');
+//     const accessToken = localStorage.getItem('accessToken');
+
+//     useEffect(() => {
+//       if (accessToken) {
+//         fetch('https://click-net-test.onrender.com/verifyToken', {
+//           method: 'GET',
+//           headers: {
+//             'Content-Type': 'application/json',
+//             'token': accessToken,
+//           },
+//         })
+//           .then((response) => {
+//             if (response.ok) {
+//               setStatus('authorized');
+//             } else {
+//               setStatus('unauthorized');
+//             }
+//           })
+//           .catch(() => {
+//             setStatus('unauthorized');
+//           });
+//       } else {
+//         setStatus('unauthorized');
+//       }
+//     }, [accessToken]);
+
+//     if (status === 'loading') {
+//       return <div>Loading...</div>;
+//     }
+
+//     if (status === 'unauthorized') {
+//       return <Unauthorized imageUrl={imageUrl} />;
+//     }
+
+//     return <WrappedComponent {...props} />;
+//   };
+// };
+
+// export default preventUnauthorized;
