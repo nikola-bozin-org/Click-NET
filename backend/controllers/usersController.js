461078f077d9b8896ca45f0059269bedb17df53f
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-03-30 08:33:41 +0200

    /

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,13 +1,7 @@
-const {User} = require('../schemas')
-const bcrypt = require('bcrypt')
 const statusCode = require('../statusCodes')
 const jwt = require('../jwt')
-const UserActions = require('../helpers/userActions')
-const UserActionDescription = require('../helpers/userActionsDescriptions')
 const {userRoles} = require('../helpers/enums')
-
-
-
+const service = require('../services/usersService')
 
 const getUsers = async (req, res) => {
     const token = req.headers.token;
@@ -15,12 +9,9 @@ const getUsers = async (req, res) => {
     const verifyResult = jwt.verify(token);
     if(!verifyResult) return res.status(statusCode.ERROR).json({error:"Invalid Token."});
     if(verifyResult.role!==userRoles.Admin && verifyResult.role!==userRoles.Employee) return res.status(statusCode.ERROR).json({error:"Not Admin or Employee."});
-    try{
-        const users = await User.find({}).sort({ createdAt: -1 });
-        return res.status(statusCode.OK).json({ users: users });
-    }catch(e){
-        return res.status(statusCode.INTERNAL_SERVER_ERROR).json({error:`Server error: ${e.message}`});
-    }
+    const result = await service._getUsers();
+    if(result.error) return res.status(statusCode.INTERNAL_SERVER_ERROR).json({error:`Server error: ${result.error}`});
+    return res.status(statusCode.OK).json({ users: result.users });
 }
 const getUser = async (req, res) => {
     const token = req.headers.token;
@@ -29,16 +20,9 @@ const getUser = async (req, res) => {
     if(!verifyResult) return res.status(statusCode.ERROR).json({error:"Invalid Token."});
     if(verifyResult.role!==userRoles.Admin && verifyResult.role!==userRoles.Employee) return res.status(statusCode.ERROR).json({error:"Not Admin or Employee."});
     const { id } = req.params;
-    const username = id;
-    try{
-        const user = await User.findOne({ username });
-        if (user === null) {
-            return res.status(statusCode.ERROR).json("User not found.");
-        }
-        res.status(statusCode.OK).json({ user: user });
-    }catch(e){
-        return res.status(statusCode.INTERNAL_SERVER_ERROR).json({error:`Server error: ${e.message}`});
-    }
+    const result = await service._getUser(id);
+    if(result.error) return res.status(statusCode.INTERNAL_SERVER_ERROR).json({error:`Server error: ${result.error}`});
+    return res.status(statusCode.OK).json({ user: result.user });
 }
 const createUser = async (req, res) => {
     const token = req.headers.token;
@@ -47,66 +31,19 @@ const createUser = async (req, res) => {
     if(!verifyResult) return res.status(statusCode.ERROR).json({error:'Invalid Token.'});
     if(verifyResult.role!==userRoles.Admin && verifyResult.role!==userRoles.Employee) return res.status(statusCode.ERROR).json({error:'you are not Admin or Employee.'});
     const { username, password, firstName, lastName, email, phone } = req.body;
-    try {
-    const user = await User.findOne({ username });
-    if (user !== null) return res.status(statusCode.ERROR).json({ error: `User with username: ${username} already exists.` });
-    const hashedPassword = bcrypt.hashSync(password, 10);
-        const createResult = await User.create({
-            username,
-             password: hashedPassword,
-              balance: 0,
-              discount: 0,
-              xp: 0,
-              role:"Default",
-            basicInfo: {
-                firstName,
-                lastName,
-                email,
-                phone
-            },
-            actions:[
-                {
-                    name:UserActions.AccountCreation,
-                    description:UserActionDescription.AccountCreation(verifyResult.username,username),
-                    date:Date.now(),
-                    pcNumber:-1,
-                    balanceChange:0
-                }
-            ],
-            activeTickets:[],
-            payments:[]
-        });
-        return res.status(statusCode.OK).json({ userCreated: true});
-    } catch (e) {
-        return res.status(statusCode.INTERNAL_SERVER_ERROR).json({error:`Server error: ${e.message}`});
-    }
+    const result = await service._createUser(verifyResult.username,username,password,firstName,lastName,email,phone);
+    if(result.error) return res.status(statusCode.INTERNAL_SERVER_ERROR).json({error:`Server error: ${result.error}`});
+    return res.status(statusCode.OK).json({ userCreated: result.userCreated});
 }
 const changePassword = async(req,res)=>{
     const token = req.headers.token;
     if(!token) return res.status(statusCode.UNAUTHORIZED).json({error:'Unauthorized.'})
     const verifyResult = jwt.verify(token);
     if(!verifyResult) return res.status(statusCode.ERROR).json({error:'Invalid token.'});
-    const username = verifyResult.username;
-    try{
-        const user = await User.findOne({username});
-        if(!user) return res.status(statusCode.ERROR).json({error:'User does not exist.'});
-        const {oldPassword,newPassword} = req.body; 
-        if(!bcrypt.compareSync(oldPassword,user.password)) return res.status(statusCode.ERROR).json({error:'Wrong password.'});
-        const hashedPassword = await bcrypt.hash(newPassword,10);
-        const updatePasswordResult = await User.findOneAndUpdate({username},{
-            password:hashedPassword,
-            $push:{
-                actions:{
-                    name:UserActions.PasswordChange,
-                    description:UserActionDescription.PasswordChange,
-                    date:Date.now(),
-                    pcNumber:verifyResult.pcNumber,
-                    balanceChange:0
-                }}});
-        return res.status(statusCode.OK).json({message:"Password changed."});
-    }catch(e){
-        return res.status(statusCode.INTERNAL_SERVER_ERROR).json({error:`Server error: ${e.message}`});
-    }
+    const {oldPassword,newPassword} = req.body; 
+    const result = await service._changePassword(verifyResult.username,oldPassword,newPassword,verifyResult.pcNumber);
+    if(result.error) return res.status(statusCode.INTERNAL_SERVER_ERROR).json({error:`Server error: ${result.error}`});
+    return res.status(statusCode.OK).json({message:result.message});
 }
 module.exports = {
     getUsers,
---
+++ backend/services/usersService.js
@@ -0,0 +1,87 @@
+const {User} = require('../schemas')
+const UserActions = require('../helpers/userActions')
+const UserActionDescription = require('../helpers/userActionsDescriptions')
+const bcrypt = require('bcrypt')
+
+
+const _getUsers = async ()=>{
+    try{
+        const users = await User.find({}).sort({ createdAt: -1 });
+        return {users:users}
+    }catch(e){
+        return {error:e.message}
+    }
+}
+const _getUser = async(username) =>{
+    try{
+        const user = await User.findOne({ username });
+        if (user === null) return {erorr:`User ${username} not found.`};
+        return { user: user };
+    }catch(e){
+        return {error:e.message}
+    }
+}
+const _createUser = async(staffName,username,password,firstName,lastName,email,phone)=>{
+    try {
+        const user = await User.findOne({ username });
+        if (user !== null) return { error: `User with username: ${username} already exists.` };
+        const hashedPassword = bcrypt.hashSync(password, 10);
+            const createResult = await User.create({
+                username,
+                 password: hashedPassword,
+                  balance: 0,
+                  discount: 0,
+                  xp: 0,
+                  role:"Default",
+                basicInfo: {
+                    firstName,
+                    lastName,
+                    email,
+                    phone
+                },
+                actions:[
+                    {
+                        name:UserActions.AccountCreation,
+                        description:UserActionDescription.AccountCreation(staffName.username,username),
+                        date:Date.now(),
+                        pcNumber:-1,
+                        balanceChange:0
+                    }
+                ],
+                activeTickets:[],
+                payments:[]
+            });
+            return {userCreated:true}
+        }catch(e){
+            return {error:e.message}
+        }
+}
+const _changePassword = async(username,oldPassword,newPassword,pcNumber) =>{
+    try{
+        const user = await User.findOne({username});
+        if(!user) return {error:`User ${username} does not exist.`};
+        if(!bcrypt.compareSync(oldPassword,user.password)) return {error:'Wrong password.'};
+        const hashedPassword = await bcrypt.hash(newPassword,10);
+        const updatePasswordResult = await User.findOneAndUpdate({username},{
+            password:hashedPassword,
+            $push:{
+                actions:{
+                    name:UserActions.PasswordChange,
+                    description:UserActionDescription.PasswordChange,
+                    date:Date.now(),
+                    pcNumber:pcNumber,
+                    balanceChange:0
+                }}});
+        return {message:'Password updated!'}
+    }catch(e){
+        return {error:e.message}
+    }
+}
+
+
+module.exports={
+    _getUsers,
+    _getUser,
+    _createUser,
+    _changePassword
+}
