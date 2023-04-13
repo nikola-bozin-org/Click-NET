68f927b136a806089c989443e435e21f15272ceb
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-04-13 09:51:36 +0200

    /

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,6 @@ const utilsRouter = require('./routers/utilsRouter')
 
 const dropDatabaseRouter = require('./routers/other/dropDatabaseRouter')
 const dummyRouter = require('./routers/other/dummyRouter');
-const checkIfViewer = require('./helpers/viewerMiddleware');
 
 
 
@@ -33,7 +32,6 @@ server.use(express.urlencoded({ extended: true }));
 server.use(mongoSanitize());
 server.use(xss());
 // server.use(hpp());
-server.use(checkIfViewer)
 server.use('/api/users',usersRouter);
 server.use('/api/session',sessionsRouter);
 server.use('/api/tickets',ticketsRouter);
