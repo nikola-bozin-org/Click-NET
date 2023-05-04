8f0edb7790a50bd84de55cc27fab3bcce790eea3
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-05-04 22:31:17 +0200

    /

--- frontend/src/clientRemoteController.js
+++ frontend/src/clientRemoteController.js
@@ -6,7 +6,7 @@ const {
 const store = require("./redux/store").default;
 
 let ws;
-const connect = (accessToken) => {
+export const connect = (accessToken) => {
     console.info(accessToken)
     ws = new WebSocket(
     `ws://localhost:${9875}/?jwt=${encodeURIComponent(accessToken)}`
@@ -21,19 +21,13 @@ const connect = (accessToken) => {
     console.info(message)
   };
 };
-const disconnect = () => {
+export const disconnect = () => {
   if (!ws) return "Unable to disconnect. (Not Connected to Websocket)";
   ws.close();
   store.dispatch(setDisconnectedFromWebsocket());
   console.log("Disconnected from server");
 };
-const sendMessage = (from, to, message) => {
+export const sendMessage = (from, to, message) => {
   if (!ws) return "Unable to disconnect. (Not Connected to Websocket)";
   ws.send("Hello");
-};
-
-module.exports = {
-  connect,
-  disconnect,
-  sendMessage,
 };
