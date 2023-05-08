ef2babc5aca3715fe36c35cc46ff727100d96d6d
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-05-08 15:26:34 +0200

    /

--- frontend/src/components/powered-by/PoweredBy.jsx
+++ frontend/src/components/powered-by/PoweredBy.jsx
@@ -5,7 +5,7 @@ const PoweredBy = () => {
   return (
     <div className='powered-by'>
         <p>
-      `203 - All rights reserved - Version: {versionData.version} 
+      2023 - All rights reserved - Version: {versionData.version} 
        ~ Powered By: <a style={{color:'white',fontSize:'18px'}} href='https://nikolabozin.com' target='_blank' rel='noopener noreferrer'>nikolabozin.com</a>
       </p>
     </div>  )
