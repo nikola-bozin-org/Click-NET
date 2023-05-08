aa22d728d7ec5a311a1ca12458b2b77d57503342
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-05-08 22:39:02 +0200

    /

--- frontend/src/components/powered-by/PoweredBy.jsx
+++ frontend/src/components/powered-by/PoweredBy.jsx
@@ -6,7 +6,7 @@ const PoweredBy = () => {
     <div className='powered-by'>
         <p>
       2023 - All rights reserved - Version: {versionData.version} 
-       ~ Powered By: <a style={{color:'white',fontSize:'18px'}} href='https://nikolabozin.com' target='_blank' rel='noopener noreferrer'>nikolabozin.com</a>
+       ~ Powered By: <a style={{color:'white',fontSize:'18px',textDecoration:'none'}} href='https://nikolabozin.com' target='_blank' rel='noopener noreferrer'>nikolabozin.com</a>
       </p>
     </div>  )
 }
